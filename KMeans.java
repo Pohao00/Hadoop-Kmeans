b5ab8e93ee0642a15fdb2b3309d894cfc41c7d19
BananaTW <ex00000001@yahoo.com.tw>
2019-07-23 20:47:57 +0800

    modify typesetting

--- KMeans.java
+++ KMeans.java
@@ -25,51 +25,49 @@ public class KMeans {
 	public static String out; //輸出結果
 	public static String in; //輸入資料
 	
-	public static class Map extends Mapper<Object, Text, DoubleWritable, DoubleWritable>{  //Input Key, Input Value, Output Key, Output Value
-    public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
-if (t == false) { //設定起始形心位置
-Centers.add(Double.parseDouble("20"));
-Centers.add(Double.parseDouble("30"));
-Centers.add(Double.parseDouble("40"));
-t = true;
-}
-
-			String inputvalue = value.toString(); //Input Value讀取
-			double point = Double.parseDouble(inputvalue); //轉換成double型態
-			double a, b = Double.MAX_VALUE, nearest_center = Centers.get(0); //設定比較參數及選定最近形心參數
-
-			// 找出最近形心
-			for (double c : Centers) {
-				a = c - point;
-				if (Math.abs(a) < Math.abs(b)) {
-					nearest_center = c;
-					b = a;
-				}
+	public static class Map extends Mapper<Object, Text, DoubleWritable, DoubleWritable> {  //Input Key, Input Value, Output Key, Output Value
+    	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			if (t == false) { //設定起始形心位置
+				Centers.add(Double.parseDouble("20") );
+				Centers.add(Double.parseDouble("30") );
+				Centers.add(Double.parseDouble("40") );
+				t = true;
 			}
+
+		String inputvalue = value.toString(); //Input Value讀取
+		double point = Double.parseDouble(inputvalue); //轉換成double型態
+		double a, b = Double.MAX_VALUE, nearest_center = Centers.get(0); //設定比較參數及選定最近形心參數
+
+		// 找出最近形心
+		for (double c : Centers) {
+			a = c - point;
+			if (Math.abs(a) < Math.abs(b) ) {
+				nearest_center = c;
+				b = a;
+			}
+		}
 			// 送出最近形心與相對應點
 			
-			context.write(new DoubleWritable(nearest_center), new DoubleWritable(point));
+		context.write(new DoubleWritable(nearest_center), new DoubleWritable(point) );
 			
 		}
-  }
+  	}
 
-  public static class Reduce extends Reducer<DoubleWritable, DoubleWritable, DoubleWritable, Text> { //Input Key, Input Value, Output Key, Output Value
-	
-    public void reduce(DoubleWritable key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+	public static class Reduce extends Reducer<DoubleWritable, DoubleWritable, DoubleWritable, Text> { //Input Key, Input Value, Output Key, Output Value
+		public void reduce(DoubleWritable key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
 			double newCenters; //新的形心參數
 			double sum = 0; //所有點的總和
 			int tatal = 0; //距離某一形心總數
 			String points = "";
 			Centers.clear(); //清理原本的形心
 
-			for (DoubleWritable valuess : values){ //印出所屬形心的集合點
+			for (DoubleWritable valuess : values) { //印出所屬形心的集合點
 				double d = valuess.get();
 				points = points + " " + Double.toString(d);
 				sum = sum + d;
 				tatal++;
 			}
 
-			
 			newCenters = sum / tatal; //更新新的形心，利用平均
 
 			Centers.add(newCenters);//放入新的形心
@@ -77,41 +75,40 @@ t = true;
 			double temp = key.get();
 			if(Math.abs( temp - newCenters) <= 0.1) { //判斷前後形心是否小於0.1
 				isdone = true;
-					}
+			}
 			else {
 				isdone = false;
-				}
+			}
 
 			// 送出最近形心與相對應點
-			context.write(new DoubleWritable(newCenters), new Text(points));
+			context.write(new DoubleWritable(newCenters), new Text(points) );
 		}
-  }
+ 	}
 
-  public static void main(String[] args) throws Exception {
-		
+  	public static void main(String[] args) throws Exception {
 
-		while(isdone == false){
-		in = args[0];
-		out = args[1];
-		String input = in;
-		String output = out;
-    Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "KMeans");
-    job.setJarByClass(KMeans.class);
-    //job.setInputFormatClass (LongWritable.class);
-    job.setMapOutputKeyClass(DoubleWritable.class);
-    job.setMapOutputValueClass(DoubleWritable.class);
+		while(isdone == false) {
+			in = args[0];
+			out = args[1];
+			String input = in;
+			String output = out;
+    		Configuration conf = new Configuration();
+    		Job job = Job.getInstance(conf, "KMeans");
+    		job.setJarByClass(KMeans.class);
+    		//job.setInputFormatClass (LongWritable.class);
+    		job.setMapOutputKeyClass(DoubleWritable.class);
+    		job.setMapOutputValueClass(DoubleWritable.class);
     
-    job.setOutputKeyClass(DoubleWritable.class);
-    job.setOutputValueClass(Text.class);
-    job.setMapperClass(Map.class);
-    //job.setCombinerClass(Reduce.class);
-    job.setReducerClass(Reduce.class);
+    		job.setOutputKeyClass(DoubleWritable.class);
+    		job.setOutputValueClass(Text.class);
+    		job.setMapperClass(Map.class);
+    		//job.setCombinerClass(Reduce.class);
+    		job.setReducerClass(Reduce.class);
     
-    FileInputFormat.addInputPath(job, new Path(input));
-    FileOutputFormat.setOutputPath(job, new Path(output));
+    		FileInputFormat.addInputPath(job, new Path(input) );
+    		FileOutputFormat.setOutputPath(job, new Path(output) );
 	
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-			}
-  }
+    		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		}
+  	}
 }
